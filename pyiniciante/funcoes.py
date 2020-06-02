725fec3350a722a80192e3857849779506c5a9f7
mrBigogno <gustavofbigogno@gmail.com>
2020-06-02 19:50:53 -0300

    codigo basico

---
+++ pyiniciante/funcoes.py
@@ -0,0 +1,17 @@
+import math
+
+graus = 45
+
+radians = graus / 180.0 * math.pi
+
+print("Seno de", graus,":", math.sin(radians))
+
+print("Seno = Raiz(2)/2:", math.sqrt(2) / 2.0)
+
+def print_lyrics():
+    print("I'm a lumberjack, and I'm okay.")
+    print("I sleep all night and I work all day.")
+
+print_lyrics()
+
+
